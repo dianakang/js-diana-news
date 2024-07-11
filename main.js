83acf0255a9ee7ddc553d7b574d2c7c3b43b73bd
dianakang <younjung952@hufs-gsuite.kr>
2024-07-11 21:03:58 +0900

    Add Hamburger menu, Search bar, News source & summary, Edit date format using momentjs and summary letter count

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
-let API_KEY = `e88b2b0a61f34f40b495de945e5045c2`;
-let newsList = []
+let API_KEY = 'e88b2b0a61f34f40b495de945e5045c2';
+let newsList = [];
 
 async function getNews() {
-    let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`);
+    let url = new URL('https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines');
     let response = await fetch(url);
     let data = await response.json();
-    newsList = data.articles; // 재할당
+    newsList = data.articles;
     render();
     console.log("dddd", newsList);
-};
-let render=()=>{
-    let newsHTML = newsList.map(
-        news=>`<div class="row news">
-        <div class="col-lg-4">
-            <img class="news-img-size"
-                src=${news.urlToImage}/>
-        </div>
-        <div class="col-lg-8">
-            <h2>${news.title}</h2>
-            <p>
-                ${news.description}
-            </p>
-            <div>
-                ${news.source.name} * ${news.publishedAt}
+}
+
+let render = () => {
+    let newsHTML = newsList.map(news => {
+        let image = news.urlToImage ? news.urlToImage : '/images/imgnotvailable.png';
+        let description = news.description ? (news.description.length > 200 ? news.description.substring(0, 200) + "..." : news.description) : "내용없음";
+        let date = moment(news.publishedAt).fromNow();
+
+        return `
+            <div class="row news">
+                <div class="col-lg-4">
+                    <img class="news-img-size" src="${image}" alt="News Image"/>
+                </div>
+                <div class="col-lg-8">
+                    <h2>${news.title}</h2>
+                    <p>${description}</p>
+                    <div>${news.source.name} * ${date}</div>
+                </div>
             </div>
-        </div>
-    </div>`).join('');
-    
+        `;
+    }).join('');
+
     document.getElementById("news-board").innerHTML = newsHTML;
 };
+
 getNews();
