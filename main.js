2fc915f69ccd8a30824a5b18b6cde1a3f88ac1ba
dianakang <younjung952@hufs-gsuite.kr>
2024-07-12 21:25:25 +0900

    Apply advanced Pagination, Edit Pagination UI

--- main.js
+++ main.js
@@ -105,34 +105,42 @@ let errorRender = (errorMessage) => {
 };
 
 let paginationRender = () => {
-  let totalPages = Math.ceil(totalResults / pageSize);
-  let pageGroup = Math.ceil(page / groupSize);
-  let lastPage = pageGroup * groupSize;
+    let totalPages = Math.ceil(totalResults / pageSize);
+    let pageGroup = Math.ceil(page / groupSize);
+    let lastPage = pageGroup * groupSize;
+  
+    if (lastPage > totalPages) {
+      lastPage = totalPages;
+    }
+  
+    let firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+    let paginationHTML = '';
+  
+    // 처음과 끝 페이지 그룹에서는 화살표 표시 안 함
+    if (pageGroup > 1) {
+      paginationHTML += `<li class="page-item" onclick="moveToPage(1)"><a class="page-link pagination-hover"><<</a></li>`;
+      paginationHTML += `<li class="page-item" onclick="moveToPage(${page - 1})"><a class="page-link pagination-hover"><</a></li>`;
+    }
+  
+    for (let i = firstPage; i <= lastPage; i++) {
+      paginationHTML += `<li class="page-item ${i === page ? "active" : ""}" onclick="moveToPage(${i})"><a class="page-link pagination-hover">${i}</a></li>`;
+    }
+  
+    if (pageGroup < Math.ceil(totalPages / groupSize)) {
+      paginationHTML += `<li class="page-item" onclick="moveToPage(${page + 1})"><a class="page-link pagination-hover">></a></li>`;
+      paginationHTML += `<li class="page-item" onclick="moveToPage(${totalPages})"><a class="page-link pagination-hover">>></a></li>`;
+    }
+  
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+  };
 
-  if (lastPage > totalPages) {
-    lastPage = totalPages;
-  }
-
-  let firstPage =
-    lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
-  let paginationHTML = `<li class="page-item" onclick="moveToPage(${
-    page - 1
-  })"><a class="page-link">Previous</a></li>`;
-
-  for (let i = firstPage; i <= lastPage; i++) {
-    paginationHTML += `<li class="page-item ${
-      i === page ? "active" : ""
-    }" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
-  }
-  paginationHTML += `<li class="page-item" onclick="moveToPage(${
-    page + 1
-  })"><a class="page-link">Next</a></li>`;
-  document.querySelector(".pagination").innerHTML = paginationHTML;
-};
-
+// 페이지 그룹에 따라 페이지 이동하는 함수
 let moveToPage = (pageNum) => {
-  console.log("movetopage", pageNum);
-  page = pageNum;
-  getNews();
-};
+    if (pageNum < 1 || pageNum > Math.ceil(totalResults / pageSize)) {
+      return;
+    }
+    page = pageNum;
+    getNews();
+  };
+
 getLatestNews();
